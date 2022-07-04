e7a804d1e4815ee23334062b799a4a50cd15baa3
Samorodov Sergey <samorodovsv@yandex.ru>
2022-07-04 18:08:36 +0300

    добавление задач 1 и 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// // 918 -> 1
+// Console.Write("Введите трехзначное число:");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int a1 = x / 10;
+// int a2 = x / 10 % 10;
+// Console.Write(x + "->" + a2);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// void SearchForTheNumberThree ( int number){ 
+// if (number < 100)
+// {
+//     Console.Write( number + "-> "+ "Третьей цифры нет");
+// }
+// else if (number >= 100 & number < 1000)
+// {
+//     int a1 = number % 10;
+//     Console.Write( number + "-> "+ a1);
+// }
+// else if (number >= 1000 & number < 10000)
+// {
+//      int a1 = number  / 10 % 10;
+//     Console.Write( number + "-> "+ a1);
+// }
+// else if (number >= 10000 & number < 100000)
+// {
+//      int a1 = number  / 100 % 10;
+//     Console.Write( number + "-> "+ a1);
+// }
+// else if (number >= 100000 & number < 1000000)
+// {
+//      int a1 = number  / 1000 % 10;
+//     Console.Write( number + "-> "+ a1);
+// }
+// else
+// {
+//     Console.Write( number + "-> "+ "Введено много символов. ");
+// }
+
+// }
+
+// Console.Write("Введите число:");
+// int x = Convert.ToInt32(Console.ReadLine());
+// SearchForTheNumberThree(x);
